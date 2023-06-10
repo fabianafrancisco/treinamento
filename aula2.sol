eb224e592934087581d878e1699a7bd05717b51c
fabianafrancisco <82617837+fabianafrancisco@users.noreply.github.com>
2023-06-10 10:28:52 -0300

    aula 2

--- aula2.sol
+++ aula2.sol
@@ -6,12 +6,17 @@ pragma solidity 0.8.20;
  * @title Ballot
  * @dev Implements voting process along with vote delegation
  */
- //0x599E80D14aC9521c875753eeeAcaC3e8ecc1E195 - deploy
+ //0x2ecF1Bb487D82E05048B19a71feF49D9500e05b4 - deploy
  //0x561bC36Eb5787565fE6c1F099D62A01163eA9171 - contrato
 contract Vendedor {
 
     string public nmVendedor;
     uint256 public vrFatorBonus;
+
+    constructor(string memory nomeVendedor, uint256 valorFatorBonus) {
+        nmVendedor = nomeVendedor;
+        vrFatorBonus = valorFatorBonus;
+    }
 
     function retornarValorBonus(uint256 vrVenda) public 
         view
